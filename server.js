49ac94136362b49e85af47d7c7e4b8094f3967c7
Julian Meinhardt <julianmeinhardt@gmail.com>
2017-05-11 16:22:28 +0200

    add mongodb, bodyparser and handle  first post

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+
 const express = require('express');
+const bodyParser= require('body-parser');
+
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+MongoClient.connect('link-to-mongodb', (err, database) => {
+  // ... start the server
+})
 
 app.listen(3000, function() {
   console.log('listening on 3000');
@@ -9,4 +20,10 @@ app.listen(3000, function() {
 app.get('/', (req, res) => {
   console.log('__dirname:' + __dirname);
   res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body);
+  res.status = 200;
+  return res;
 })
